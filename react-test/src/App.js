c86f289cee6164fb4ede2b9b0aaae65847723ba8
ddxg12 <kdg332211@gmail.com>
2025-05-08 16:47:29 +0900

    5.8

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,75 +1,97 @@
-import MyButton from "./MyButton"
-import { Button1, Button2, Button3 } from "./Buttonlib"
-import AP from "./AboutPage"
-import { Profiler, useState } from "react";
-import AboutPage from "./AboutPage";
-import CountState from "./CountState";
-
 export default function App() {
   return (
-    <div>
-    <h1>Hello React</h1>
-    <MyButton /><br />
-    <Button1 />&nbsp;
-    <Button3 />
-    <AP/>
-    </div>
-  );
-}
-
-export default function App() {
-  const [count, setCount] = useState(0)
-
-  function handleClick() {
-    setCount(count + 1)
-  }
-
-  return (
-    <div>
-      <button onClick={onclick}>
-      Clicked [count] times
-      </button>
-    </div>
-  );
-}
-
-export default function App() {
-  const [count, setCount] = useState(0)
-
-  function handleClick() {
-    setCount(count + 1)
-  }
-
-  return (
-    <div className='wrapper'>
-      <h1>Welcome to my app</h1>
-    <div>
-      <p>default export example</p>
-      <MyB />
-      <p>default export example</p>
-      <Button1 /> &nbsp;
-      <Button3 />
-      <p>wrapping example</p>
-      <AboutPage />
-      <p>Displaying data</p>
-      <Profile />
-      <p>Rendering lists</p>
-      <ShoppingList />
-    </div>
-    <div>
-      <p>Updating the screen</p>
-      <CountState />
-      <CountState />
-      <CountState />
-    </div>
-    <div>
-      <p>Sharing data between component</p>
-      <CountState2 count= {count} onClick={handleClick} />
-      <CountState2 count= {count} onClick={handleClick} />
-    </div>
-  </div>  
+    <>
+      <FilterableProductTable products={PRODUCTS} />
+    </>
   )
 }
+
+function FilterableProductTable({ products }) {
+  return
+  <div>
+    <SearchBar />
+    <ProductTable products={products} />
+  </div>
+  );
+}
+
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        {' '}
+        only show products in stock
+        </label>
+      </form>
+    );
+}
+
+function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+  );
+}
+function ProductRow({ product }) {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+const PRODUCTS = [
+  {category: "frwits", price: "$1", stocked: true, name: "Apple"},
+  {category: "frwits", price: "$1", stocked: true, name: "Orangefruit"},
+  {category: "frwits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"},
+]
 
 
 
